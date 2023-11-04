8b38d798eee92343195693ffc61860dc0e5ae784
Moamen777 <moamendev77@gmail.com>
2023-11-04 14:35:34 +0200

    JavaScript intro

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+window.alert(" ادخل طول ضلع المربع حتى يتم احتساب مساحته ومحيطه");
+ var number = window.prompt("ادخل طول ضلع المربع");
+ var result1 = number * number
+ var result2 = number * 4
+window.alert("مساحة المربع" + result1);
+console.log("مساحة المربع" + result1)
+window.alert("محيط المربع" + result2);
+console.log("محيط المربع" + result2)
